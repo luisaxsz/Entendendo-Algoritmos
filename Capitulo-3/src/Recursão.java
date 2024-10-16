c6174a7f219adb8f9b466cac6ba1b47c6aeca0ff
luisa lameirao <luisa.lameirao@tre-pa.jus.br>
2024-10-16 16:32:11 -0300

    feat: Recursão soma

---
+++ Capitulo-3/src/Recursão.java
@@ -0,0 +1,24 @@
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
+
+public class Recursão {
+    public static void main(String[] args) {
+        System.out.println(fatorial(5));
+        System.out.println(soma(new ArrayList<>(java.util.List.of(2,4,6))));
+    }
+
+    public static int fatorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        System.out.println(n);
+        return n * fatorial(n - 1);
+    }
+
+    public static int soma(ArrayList<Integer> lista){
+        if (lista.size() == 0){
+            return 0;
+        }
+        return lista.get(0) + soma(new ArrayList<>(lista.subList(1, lista.size())));
+    }
+}
